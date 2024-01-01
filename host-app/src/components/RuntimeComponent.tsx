3f0e6defd0f44088864be1e8a73221b4e7816f8d
PatrickJS <github@patrickjs.com>
2024-01-01 10:47:04 -0800

    refactor: cache maybe

--- host-app/src/components/RuntimeComponent.tsx
+++ host-app/src/components/RuntimeComponent.tsx
@@ -10,15 +10,61 @@ import {
   useVisibleTask$,
 } from "@builder.io/qwik";
 
+import { isBrowser, isServer } from "@builder.io/qwik/build";
+
 export const RuntimeContext = createContextId<{ origin: string }>(
   "RuntimeContext"
 );
+
+// batch fetches based on 10ms
+let cache: Cache | null = null;
+
+async function getCache(url: string) {
+  if (isServer) {
+    return null;
+  }
+  if (cache === null) {
+    cache = await window.caches.open("remote-container-components");
+  }
+  const res = await cache.match(url);
+  if (res) {
+    return res as Response;
+  }
+  return null;
+}
+
+async function cacheFetch(url: string, fakeLoading = false) {
+  if (isBrowser) {
+    if (cache === null) {
+      cache = await window.caches.open("remote-container-components");
+    }
+    const res = await cache.match(url);
+    if (res) {
+      return res;
+    }
+  }
+  const res = await fetch(url);
+  if (fakeLoading) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+  if (isBrowser && cache) {
+    await cache.put(url, res.clone());
+  }
+  return res;
+}
 
 export const RuntimeComponent = component$(
   ({ name, clientOnly, fallback, origin = "", fakeLoading, ...props }) => {
     const contextConfig = useContext(RuntimeContext);
     const originUrl = contextConfig.origin || origin;
     const csr = useSignal(false);
+    const hostElement = "div";
+    const attrs: any = [];
+    // add track if you want the component to be different on the client
+    const config = `?component=${name}&tagName=${hostElement}&attributes=${attrs.join(
+      ","
+    )}`;
+    const url = `${originUrl}${originUrl.endsWith("/") ? "" : "/"}${config}`;
 
     // eslint-disable-next-line qwik/no-use-visible-task
     useVisibleTask$(() => {
@@ -26,39 +72,34 @@ export const RuntimeComponent = component$(
     });
 
     const remoteCmp = useResource$(async ({ track }) => {
-      if (clientOnly === true && csr.value === false) {
+      track(() => clientOnly);
+      if (clientOnly === true) {
         track(() => csr.value);
-        return;
-      } else if ((clientOnly === true && csr.value) === true) {
-        // fake loading
-        if (fakeLoading) {
-          await new Promise((resolve) => setTimeout(resolve, 1000));
+        const cache = await getCache(url);
+        if (cache !== null) {
+          return await cache.text();
         }
+        if (csr.value === false) return;
       }
-      const hostElement = "div";
-      const attrs: any = [];
-      // add track if you want the component to be different on the client
-      const config = `?component=${name}&tagName=${hostElement}&attributes=${attrs.join(
-        ","
-      )}`;
       // send outerHTML to replace
       // get URL from config
       // can get default config from fetch or window.config etc
-      const res = await fetch(
-        `${originUrl}${originUrl.endsWith("/") ? "" : "/"}${config}`
-      );
+      const res = await cacheFetch(url, fakeLoading);
       // cache response
       const html = await res.text();
       return html;
     });
-    return clientOnly === true && fallback && csr.value === false ? (
-      fallback
-    ) : (
+    return (
       <Resource
         value={remoteCmp}
-        onResolved={(html) => (
-          /* need outerHTML */ <div dangerouslySetInnerHTML={html} {...props} />
-        )}
+        onPending={() => fallback}
+        onResolved={(html) =>
+          /* need outerHTML */ !html ? (
+            fallback
+          ) : (
+            <div dangerouslySetInnerHTML={html} {...props} />
+          )
+        }
       />
     );
   }
